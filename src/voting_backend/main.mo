751a8b764cff25677481fca4053b4bb43f0a9f06
capy-on-caffeine <arnav.anand2004@gmail.com>
2023-09-08 11:04:45 +0530

    basic changes to code, incorrect version

--- src/voting_backend/main.mo
+++ src/voting_backend/main.mo
@@ -1,5 +1,61 @@
+import RBTree "mo:base/RBTree";
+import Nat "mo:base/Nat";
+import Text "mo:base/Text";
+import Iter "mo:base/Iter";
+import Int "mo:base/Int";
+import Char "mo:base/Char";
+import Nat32 "mo:base/Nat32";
+import Option "mo:base/Option";
+
+
 actor {
-  public query func greet(name : Text) : async Text {
-    return "Hello, " # name # "!";
+  let psk : Text = "theorywillonlytakeyousofar";
+
+  var question : Text = "Who will sit on the throne?";
+  var votes : RBTree.RBTree<Text, Nat> = RBTree.RBTree(Text.compare);
+
+  func textToNat(t : Text) : Nat {
+    var n : Nat = 0;
+    for (c in t.chars()) {
+      if (Char.isDigit(c)) {
+        let charAsNat : Nat = Nat32.toNat(Char.toNat32(c) - 48);
+        n := n * 10 + charAsNat;
+      }
+    };
+    return n;
   };
+
+  public query func getQuestion() : async Text {
+    question;
+  };
+
+  public query func getVotes() : async [(Text, Nat)] {
+    Iter.toArray(votes.entries());
+  };
+
+  public func addCandidate(entry : Text) : async [(Text, Nat)] {
+    votes.put(entry, 0);
+    Iter.toArray(votes.entries());
+  };
+
+  public func vote(entry : Text) : async [(Text, Nat)] {
+    let votes_for_entry : ?Nat = votes.get(entry);
+
+    let current_votes_for_entry : Int = switch votes_for_entry {
+      case null -1;
+      case (?Nat) Nat;
+    };
+
+    if (current_votes_for_entry != -1) {
+      let current_votes_for_entry_nat : Nat = textToNat(Int.toText(current_votes_for_entry));
+      votes.put(entry, current_votes_for_entry_nat + 1);
+    };
+
+    Iter.toArray(votes.entries());
+  };
+
+  public func resetVotes(passkey : Text) : async () {
+    if (passkey == psk) votes := RBTree.RBTree(Text.compare);
+  };
+
 };
--- src/voting_frontend/src/index.js
+++ src/voting_frontend/src/index.js
@@ -1,19 +1,78 @@
-import { voting_backend } from "../../declarations/voting_backend";
+const pollForm = document.getElementById("radioForm");
+const resultsDiv = document.getElementById('results');
+// const resetButton = document.getElementById('reset');
 
-document.querySelector("form").addEventListener("submit", async (e) => {
+import { poll_backend } from "../../declarations/poll_backend";
+
+const pollResults = {
+    // "Rust": 0,
+    // "Motoko": 0,
+    // "TypeScript": 0,
+    // "Python": 0
+};
+
+document.addEventListener('DOMContentLoaded', async (e) => {
   e.preventDefault();
-  const button = e.target.querySelector("button");
-
-  const name = document.getElementById("name").value.toString();
-
-  button.setAttribute("disabled", true);
-
-  // Interact with foo actor, calling the greet method
-  const greeting = await voting_backend.greet(name);
-
-  button.removeAttribute("disabled");
-
-  document.getElementById("greeting").innerText = greeting;
-
+  const question = await poll_backend.getQuestion();
+  document.getElementById("question").innerText = question;
+  
+  const voteCounts = await poll_backend.getVotes();
+  updateLocalVoteCounts(voteCounts);
+  displayResults();
   return false;
-});
+}, false);
+
+// pollForm.addEventListener('submit', async (e) => {
+//   e.preventDefault(); 
+
+//   const formData = new FormData(pollForm);
+//   const checkedValue = formData.get("option");
+
+//   const updatedVoteCounts = await poll_backend.vote(checkedValue);
+//   console.log("Returning from await...")
+//   console.log(updatedVoteCounts);
+//   updateLocalVoteCounts(updatedVoteCounts);
+//   displayResults();
+//   return false;
+// }, false);
+
+async function vote() {
+  const formData = new FormData(pollForm);
+  const checkedValue = formData.get("option");
+
+  const updatedVoteCounts = await poll_backend.vote(checkedValue);
+  console.log("Returning from await...")
+  console.log(updatedVoteCounts);
+  updateLocalVoteCounts(updatedVoteCounts);
+  displayResults();
+}
+
+const btnDiv = document.querySelector('.btn');
+btnDiv.addEventListener('click', vote);
+
+async function reset(passkey) {
+  await poll_backend.resetVotes(passkey);
+  const voteCounts = await poll_backend.getVotes();
+  updateLocalVoteCounts(voteCounts);
+  displayResults();
+};
+
+function displayResults() {
+  let resultHTML = '<ul>';
+  for (let key in pollResults) {
+      resultHTML += '<li><strong>' + key + '</strong>: ' + pollResults[key] + '</li>';
+  }
+  resultHTML += '</ul>';
+  resultsDiv.innerHTML = resultHTML;
+};
+
+function updateLocalVoteCounts(arrayOfVoteArrays){
+
+  for (let voteArray of arrayOfVoteArrays) {
+    //Example voteArray -> ["Motoko","0"]
+    let voteOption = voteArray[0];
+    let voteCount = voteArray[1];
+    pollResults[voteOption] = voteCount;
+  }
+
+};
