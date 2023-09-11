b6ea5f04a8fec5254feb3e1dc3159e75c744468b
capy-on-caffeine <arnav.anand2004@gmail.com>
2023-09-11 22:11:56 +0530

    latest changes

--- src/voting_backend/main.mo
+++ src/voting_backend/main.mo
@@ -6,10 +6,14 @@ import Int "mo:base/Int";
 import Char "mo:base/Char";
 import Nat32 "mo:base/Nat32";
 import Option "mo:base/Option";
+import Buffer "mo:base/Buffer";
+
 
 
 actor {
   let psk : Text = "theorywillonlytakeyousofar";
+
+  var votedList = Buffer.Buffer<Text>(120);
 
   var question : Text = "Who will sit on the throne?";
   var votes : RBTree.RBTree<Text, Nat> = RBTree.RBTree(Text.compare);
@@ -25,9 +29,34 @@ actor {
     return n;
   };
 
+  func personExistsVoters(e : Text) : Bool {
+    for(p in votedList.vals()) {
+      if (p == e) return true;
+    };
+    return false;
+  };
+
   public query func getQuestion() : async Text {
     question;
   };
+
+  // public func vote(voterMail : Text, candidateMail : Text) : async Text {
+  //     if (not personExistsElectionPool(voterMail)) {return "Person not in election pool";}
+  //     else if (personExistsVoters(voterMail)) {return "Person has already voted";}
+  //     else if (personExistsCandidates(voterMail)) {return "The person is an existing candidate";}
+  //     else {
+  //       for(c in candidates.vals()) {
+  //         if (c.email == candidateMail) {
+  //           c.votes += 1;
+  //           var v : Voter = {email = voterMail; hasVoted = true};
+  //           voters.add(v);
+  //         }
+  //       };
+  //       // return "No such candidate found";
+  //       return "Vote cast";
+  //     };
+  //   };
+
 
   public query func getVotes() : async [(Text, Nat)] {
     Iter.toArray(votes.entries());
@@ -38,7 +67,8 @@ actor {
     Iter.toArray(votes.entries());
   };
 
-  public func vote(entry : Text) : async [(Text, Nat)] {
+  public func vote(voterMail : Text, entry : Text) : async [(Text, Nat)] {
+    if (not personExistsVoters(voterMail)) {
     let votes_for_entry : ?Nat = votes.get(entry);
 
     let current_votes_for_entry : Int = switch votes_for_entry {
@@ -51,7 +81,10 @@ actor {
       votes.put(entry, current_votes_for_entry_nat + 1);
     };
 
+    votedList.add(voterMail);
+
     Iter.toArray(votes.entries());
+    } else return Iter.toArray(votes.entries());
   };
 
   public func resetVotes(passkey : Text) : async () {
--- src/voting_frontend/src/index.js
+++ src/voting_frontend/src/index.js
@@ -1,61 +1,86 @@
-const pollForm = document.getElementById("radioForm");
-const resultsDiv = document.getElementById('results');
-// const resetButton = document.getElementById('reset');
+import { voting_backend } from "../../declarations/voting_backend";
 
-import { poll_backend } from "../../declarations/poll_backend";
+const pollForm = document.getElementById("radioForm");
+const resultsDiv = document.getElementById('result-info');
+
+alert("working");
 
 const pollResults = {
-    // "Rust": 0,
-    // "Motoko": 0,
-    // "TypeScript": 0,
-    // "Python": 0
+    "Anoop Saini": 0,
+    "Ayush Gupta": 0,
+    "Gorantla Srinivas": 0
 };
+
+const electionEndTime = new Date("2023-09-13T00:00:00");
 
 document.addEventListener('DOMContentLoaded', async (e) => {
   e.preventDefault();
-  const question = await poll_backend.getQuestion();
-  document.getElementById("question").innerText = question;
+  // const question = await voting_backend.getQuestion();
+  // document.getElementById("question").innerText = question;
   
-  const voteCounts = await poll_backend.getVotes();
+  const voteCounts = await voting_backend.getVotes();
   updateLocalVoteCounts(voteCounts);
   displayResults();
+  voteListUpdate();
   return false;
 }, false);
 
-// pollForm.addEventListener('submit', async (e) => {
-//   e.preventDefault(); 
+pollForm.addEventListener('submit', async (e) => {
+  e.preventDefault(); 
 
-//   const formData = new FormData(pollForm);
-//   const checkedValue = formData.get("option");
-
-//   const updatedVoteCounts = await poll_backend.vote(checkedValue);
-//   console.log("Returning from await...")
-//   console.log(updatedVoteCounts);
-//   updateLocalVoteCounts(updatedVoteCounts);
-//   displayResults();
-//   return false;
-// }, false);
-
-async function vote() {
-  const formData = new FormData(pollForm);
+  const formData = new FormData(radioForm);
   const checkedValue = formData.get("option");
+  let email = document.getElementById('email-field').value;
 
-  const updatedVoteCounts = await poll_backend.vote(checkedValue);
+  const updatedVoteCounts = await voting_backend.vote(email, checkedValue);
   console.log("Returning from await...")
   console.log(updatedVoteCounts);
   updateLocalVoteCounts(updatedVoteCounts);
-  displayResults();
-}
+  // displayResults();
+  return false;
+}, false);
 
-const btnDiv = document.querySelector('.btn');
-btnDiv.addEventListener('click', vote);
+// async function vote() {
+//   const formData = new FormData(radioForm);
+//   const checkedValue = formData.get("option");
+//   let email = document.getElementById('email-field').value;
+
+//   const updatedVoteCounts = await voting_backend.vote(email, checkedValue);
+//   console.log("Returning from await...")
+//   console.log(updatedVoteCounts);
+//   updateLocalVoteCounts(updatedVoteCounts);
+//   // displayResults();
+// }
+
+// const btnDiv = document.querySelector('.btn');
+// btnDiv.addEventListener('click', vote);
+
+
+
+
+
 
 async function reset(passkey) {
-  await poll_backend.resetVotes(passkey);
-  const voteCounts = await poll_backend.getVotes();
+  await voting_backend.resetVotes(passkey);
+  const voteCounts = await voting_backend.getVotes();
   updateLocalVoteCounts(voteCounts);
   displayResults();
 };
+
+document.getElementById('email-btn').onclick = checkEmail();
+
+function checkEmail() {
+  let email = document.getElementById('email-field').value;
+  console.log(email);// remove...............................................................
+  let messageElem = document.getElementById('validation-message');
+
+  if (email.includes('.it.23@nitj.ac.in')) {
+    messageElem.innerText = 'Invalid Email!';
+  } else {
+    messageElem.innerText = 'Validated!';
+  }
+}
+// Done-----------------------------------------------------------------
 
 function displayResults() {
   let resultHTML = '<ul>';
@@ -65,6 +90,40 @@ function displayResults() {
   resultHTML += '</ul>';
   resultsDiv.innerHTML = resultHTML;
 };
+// done------------------------------------------------------------------------
+
+function voteListUpdate() {
+
+  for (let key in pollResults) {
+    const label = document.createElement("label");
+
+const radioInput = document.createElement("input");
+radioInput.type = "radio";
+radioInput.name = "option";
+radioInput.value = key;
+
+label.textContent = " " + key;
+
+label.appendChild(radioInput);
+
+targetform = document.getElementById('radioForm');
+targetform.appendChild(label);
+
+targetform.appendChild(document.createElement("br"));
+  }
+
+  const button = document.createElement("button");
+
+// Set the button type attribute to "submit"
+button.setAttribute("type", "submit");
+
+// Set the button text content to "Vote"
+button.textContent = "Vote";
+
+// Append the button to the document body or another desired element
+targetform.appendChild(button);
+}
+// done------------------------------------------------------------------
 
 function updateLocalVoteCounts(arrayOfVoteArrays){
 
@@ -76,3 +135,23 @@ function updateLocalVoteCounts(arrayOfVoteArrays){
   }
 
 };
+
+function updateTimer() {
+  const currentTime = new Date();
+  const timeRemaining = electionEndTime - currentTime;
+
+  if (timeRemaining > 0) {
+      const days = Math.floor(timeRemaining / (1000 * 60 * 60 * 24));
+      const hours = Math.floor((timeRemaining % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+      const minutes = Math.floor((timeRemaining % (1000 * 60 * 60)) / (1000 * 60));
+      const seconds = Math.floor((timeRemaining % (1000 * 60)) / 1000);
+
+      const timerMessage = `Election ends in ${days}d ${hours}h ${minutes}m ${seconds}s`;
+      document.getElementById("result-info").textContent = timerMessage;
+  } else {
+      displayResults();
+  }
+  console.log("done");
+}
+
+setInterval(updateTimer, 1000)
