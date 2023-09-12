8f40ba680892a2afcde75007422242dedfbb65c7
capy-on-caffeine <arnav.anand2004@gmail.com>
2023-09-12 11:21:38 +0530

    removing unnecessary comments

--- src/voting_backend/main.mo
+++ src/voting_backend/main.mo
@@ -8,14 +8,11 @@ import Nat32 "mo:base/Nat32";
 import Option "mo:base/Option";
 import Buffer "mo:base/Buffer";
 
-
-
 actor {
   let psk : Text = "theorywillonlytakeyousofar";
 
   var votedList = Buffer.Buffer<Text>(120);
 
-  // var question : Text = "Who will sit on the throne?";
   var votes : RBTree.RBTree<Text, Nat> = RBTree.RBTree(Text.compare);
 
   func textToNat(t : Text) : Nat {
@@ -36,36 +33,9 @@ actor {
     return false;
   };
 
-  // public query func getQuestion() : async Text {
-  //   question;
-  // };
-
-  // public func vote(voterMail : Text, candidateMail : Text) : async Text {
-  //     if (not personExistsElectionPool(voterMail)) {return "Person not in election pool";}
-  //     else if (personExistsVoters(voterMail)) {return "Person has already voted";}
-  //     else if (personExistsCandidates(voterMail)) {return "The person is an existing candidate";}
-  //     else {
-  //       for(c in candidates.vals()) {
-  //         if (c.email == candidateMail) {
-  //           c.votes += 1;
-  //           var v : Voter = {email = voterMail; hasVoted = true};
-  //           voters.add(v);
-  //         }
-  //       };
-  //       // return "No such candidate found";
-  //       return "Vote cast";
-  //     };
-  //   };
-
-
   public query func getVotes() : async [(Text, Nat)] {
     Iter.toArray(votes.entries());
   };
-
-  // public func addCandidate(entry : Text) : async [(Text, Nat)] {
-  //   votes.put(entry, 0);
-  //   Iter.toArray(votes.entries());
-  // };
 
   public func vote(voterMail : Text, entry : Text) : async [(Text, Nat)] {
     if (not personExistsVoters(voterMail)) {
@@ -77,11 +47,6 @@ actor {
     };
 
     votes.put(entry, current_votes_for_entry + 1);
-
-    // if (current_votes_for_entry != -1) {
-    //   let current_votes_for_entry_nat : Nat = textToNat(Int.toText(current_votes_for_entry));
-    //   votes.put(entry, current_votes_for_entry_nat + 1);
-    // };
 
     votedList.add(voterMail);
 
--- src/voting_frontend/src/index.js
+++ src/voting_frontend/src/index.js
@@ -3,8 +3,6 @@ import { voting_backend } from "../../declarations/voting_backend";
 const pollForm = document.getElementById("radioForm");
 const resultsDiv = document.getElementById('result-info');
 
-// alert("working");
-
 const pollResults = {
     "Anoop Saini": 0,
     "Ayush Gupta": 0,
@@ -15,13 +13,9 @@ const electionEndTime = new Date("2023-09-12T14:30:00");
 
 document.addEventListener('DOMContentLoaded', async (e) => {
   e.preventDefault();
-  // const question = await voting_backend.getQuestion();
-  // document.getElementById("question").innerText = question;
   
   const voteCounts = await voting_backend.getVotes();
   updateLocalVoteCounts(voteCounts);
-  // displayResults(); // delete if causes problems -----------------------------------------------------------
-  // voteListUpdate();
   return false;
 }, false);
 
@@ -41,30 +35,8 @@ pollForm.addEventListener('submit', async (e) => {
   updateLocalVoteCounts(updatedVoteCounts);
   }
   }
-
-  
-  
-  // displayResults();
   return false;
 }, false);
-
-// async function vote() {
-//   const formData = new FormData(radioForm);
-//   const checkedValue = formData.get("option");
-//   let email = document.getElementById('email-field').value;
-
-//   const updatedVoteCounts = await voting_backend.vote(email, checkedValue);
-//   console.log("Returning from await...")
-//   console.log(updatedVoteCounts);
-//   updateLocalVoteCounts(updatedVoteCounts);
-//   // displayResults();
-// }
-
-// const btnDiv = document.querySelector('.btn');
-// btnDiv.addEventListener('click', vote);
-
-
-
 
 
 
@@ -75,21 +47,6 @@ async function reset(passkey) {
   displayResults();
 };
 
-// document.getElementById('email-btn').onclick = checkEmail();
-
-// function checkEmail() {
-//   let email = document.getElementById('email-field').value;
-//   console.log(email);// remove...............................................................
-//   let messageElem = document.getElementById('validation-message');
-
-//   if (email.includes('.it.23@nitj.ac.in')) {
-//     messageElem.innerText = 'Invalid Email!';
-//   } else {
-//     messageElem.innerText = 'Validated!';
-//   }
-// }
-// // Done-----------------------------------------------------------------
-
 function displayResults() {
   let resultHTML = '<ul>';
   for (let key in pollResults) {
@@ -98,45 +55,10 @@ function displayResults() {
   resultHTML += '</ul>';
   resultsDiv.innerHTML = resultHTML;
 };
-// done------------------------------------------------------------------------
-
-// function voteListUpdate() {
-
-//   for (let key in pollResults) {
-//     const label = document.createElement("label");
-
-// const radioInput = document.createElement("input");
-// radioInput.type = "radio";
-// radioInput.name = "option";
-// radioInput.value = key;
-
-// label.textContent = " " + key;
-
-// label.appendChild(radioInput);
-
-// targetform = document.getElementById('radioForm');
-// targetform.appendChild(label);
-
-// targetform.appendChild(document.createElement("br"));
-//   }
-
-//   const button = document.createElement("button");
-
-// // Set the button type attribute to "submit"
-// button.setAttribute("type", "submit");
-
-// // Set the button text content to "Vote"
-// button.textContent = "Vote";
-
-// // Append the button to the document body or another desired element
-// targetform.appendChild(button);
-// }
-// done------------------------------------------------------------------
 
 function updateLocalVoteCounts(arrayOfVoteArrays){
 
   for (let voteArray of arrayOfVoteArrays) {
-    //Example voteArray -> ["Motoko","0"]
     let voteOption = voteArray[0];
     let voteCount = voteArray[1];
     pollResults[voteOption] = voteCount;
